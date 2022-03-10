24b002394610feed80a4e4ce22b457b48ba52bbb
chrissileinus <chrissileinus@googlemail.com>
2022-03-10 13:54:50 +0000

    sdgs

---
+++ src/Command.php
@@ -0,0 +1,306 @@
+<?php
+/*
+ * Created on Wed Feb 23 2022
+ *
+ * Copyright (c) 2022 Christian Backus (Chrissileinus)
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Chrissileinus\React\SQLite;
+
+/**
+ * A pool of React\MySQL connections with additional commandset like Query, Insert.....
+ */
+class Command
+{
+  static private Connection $connection;
+
+  /**
+   * Initialize the connections
+   *
+   * @param  string $uri
+   * @param  int    $poolSize
+   * @param  [type] $connectionSelector
+   * @return void
+   */
+  static function init(string $dbFile, callable $onError = null, string $schemaFile = null)
+  {
+    $dbFileExist = !file_exists($dbFile);
+
+    self::$connection = new Connection($dbFile, $onError);
+
+    if ($dbFileExist && $schemaFile && file_exists($schemaFile)) {
+      $sqlque = explode(";", preg_replace(
+        [
+          "/ ENGINE=\w+/",
+          "/--.*\n/",
+          "/\n/",
+          "/, +UNIQUE KEY[^\(]+\([^\)]+\)/",
+          "/, +KEY[^\(]+\([^\)]+\)/",
+          "/ AUTO_INCREMENT=\d+/",
+          "/ AUTO_INCREMENT/",
+        ],
+        "",
+        file_get_contents($schemaFile)
+      ));
+      $sqlque = array_filter($sqlque, function ($entry) {
+        return !!strlen($entry);
+      });
+
+      foreach ($sqlque as $sql) {
+        self::query($sql)->then(function (\Clue\React\SQLite\Result $result) {
+          echo "Query {$result->insertId} OK, {$result->changed} row(s) changed" . PHP_EOL;
+        });
+      }
+    }
+  }
+
+  static function ping(): \React\Promise\PromiseInterface
+  {
+    return self::query('PRAGMA encoding');
+  }
+
+  /**
+   * Performs an async query.
+   * 
+   * This method returns a promise that will resolve with a `QueryResult` on
+   * success or will reject with an `Exception` on error. 
+   *
+   * @param  string                          $query
+   * @return \React\Promise\PromiseInterface
+   */
+  static function query(string $sql): \React\Promise\PromiseInterface
+  {
+    return self::$connection->query($sql);
+  }
+
+  /**
+   * Prepare a insert statment and performs an async query.
+   * 
+   * This method returns a promise that will resolve with a `QueryResult` on
+   * success or will reject with an `Exception` on error. 
+   *
+   * @param  string                          $table   Table name
+   * @param  array                           $inserts A array with associative arrays with matching keys and values
+   * @param  array|null                      $indexes A array of indexes to perform "ON DUPLICATE KEY UPDATE"
+   * @return \React\Promise\PromiseInterface
+   */
+  static function insert(string $table, array $inserts, array $indexes = null): \React\Promise\PromiseInterface
+  {
+    if (array_depth($inserts) < 2) $inserts = [$inserts];
+
+    $fields = (function () use ($inserts) {
+      $t = [];
+      foreach (array_keys($inserts[0]) as $key => $field) {
+        $t[] = "`{$field}`";
+      }
+      return implode(", ", $t);
+    })();
+
+    $values = (function () use ($inserts) {
+      $t = [];
+      foreach ($inserts as $entry) {
+        $tt = [];
+        foreach (array_values($entry) as $key => $value) {
+          $tt[] = quote($value);
+        }
+        $tt = implode(", ", $tt);
+
+        $t[] = "( {$tt} )";
+      }
+      return implode(",\n ", $t);
+    })();
+
+    $updates = (function () use ($inserts, $indexes) {
+      if (!$indexes || !count($indexes)) return "";
+
+      $t = [];
+      foreach (array_keys($inserts[0]) as $field) {
+        if (array_search($field, $indexes) === false) {
+          $t[] = "`{$field}` = excluded.{$field}";
+        }
+      }
+      return "\n ON CONFLICT(" . implode(", ", $indexes) . ")\n DO UPDATE SET\n" . implode(",\n ", $t);
+    })();
+
+    $query = "INSERT\n INTO `{$table}`\n ( {$fields} )\n VALUES\n {$values}{$updates};";
+
+    // \Chrissileinus\React\Log\Writer::debug("SQL:" . PHP_EOL . $query, 'DataBase');
+    return self::query($query);
+  }
+
+  /**
+   * Prepare a update statment and performs an async query.
+   * 
+   * This method returns a promise that will resolve with a `QueryResult` on
+   * success or will reject with an `Exception` on error. 
+   *
+   * @param  string                          $table Table name
+   * @param  array                           $set   A associative arrays with matching keys and values
+   * @param  array                           $where
+   * @param  int|array                       $limit A int as limit or a array like [int offset, int limit]
+   * @return \React\Promise\PromiseInterface
+   */
+  static function update(string $table, array $set, array $where, $limit = null): \React\Promise\PromiseInterface
+  {
+    $sqlWhere = self::genWhere($where);
+    $sqlLimit = self::genLimit($limit);
+
+    $sqlSets = (function () use ($set) {
+      $t = [];
+      foreach ($set as $key => $value) {
+        if (preg_match('/\w+\(\)$/', $value, $_)) {
+          $t[] = "`{$key}` = " . $value;
+          continue;
+        }
+
+        $t[] = "`{$key}` = " . quote($value);
+      }
+      return implode(",\n  ", $t);
+    })();
+
+    $query = "UPDATE `{$table}`\n SET\n  {$sqlSets}{$sqlWhere}{$sqlLimit};";
+
+    // \Chrissileinus\React\Log\Writer::debug("SQL:" . PHP_EOL . $query, 'DataBase');
+    return self::query($query);
+  }
+
+  /**
+   * Prepare a delete statment and performs an async query.
+   * 
+   * This method returns a promise that will resolve with a `QueryResult` on
+   * success or will reject with an `Exception` on error. 
+   *
+   * @param  string                          $table Table name
+   * @param  array                           $where 
+   * @param  int|array                       $limit A int as limit or a array like [int offset, int limit]
+   * @return \React\Promise\PromiseInterface
+   */
+  static function delete(string $table, array $where, $limit = null): \React\Promise\PromiseInterface
+  {
+    $sqlWhere = self::genWhere($where);
+    $sqlLimit = self::genLimit($limit);
+
+    $query = "DELETE\n FROM `{$table}`{$sqlWhere}{$sqlLimit};";
+
+    // \Chrissileinus\React\Log\Writer::debug("SQL:" . PHP_EOL . $query, 'DataBase');
+    return self::query($query);
+  }
+
+  /**
+   * Prepare a select statment and performs an async query.
+   * 
+   * This method returns a promise that will resolve with a `QueryResult` on
+   * success or will reject with an `Exception` on error. 
+   *
+   * @param  string                          $table  Table name
+   * @param  string|array                    $fields A array of selected Fields or "*"
+   * @param  array|null                      $where
+   * @param  string|array                    $order
+   * @param  int|array                       $limit  A int as limit or a array like [int offset, int limit]
+   * @return \React\Promise\PromiseInterface
+   */
+  static function select(string $table, $fields = null, array $where = null, $order = null, $limit = null): \React\Promise\PromiseInterface
+  {
+    $sqlFields = (function () use ($fields) {
+      if (is_string($fields)) return "`{$fields}`";
+      if (is_array($fields)) return implode(", ", array_map(function ($field) {
+        return "`{$field}`";
+      }, $fields));
+      return "*";
+    })();
+
+    $sqlWhere = self::genWhere($where);
+
+    $sqlOrder = (function () use ($order) {
+      $assembleEntry = function ($order) {
+        if (preg_match("/^(?<field>[^\[\]]+)\[(?<direction>.+)\]$/", $order, $match)) {
+          extract($match);
+          $direction = isset($direction) ? $direction : "";
+          return "`{$field}` {$direction}";
+        }
+        return "`{$order}`";
+      };
+
+      $pre = "\n ORDER BY ";
+      if (is_string($order)) return $pre . $assembleEntry($order);
+      if (is_array($order)) return $pre . implode(", ", array_map($assembleEntry, $order));
+      return "";
+    })();
+
+    $sqlLimit = self::genLimit($limit);
+
+    $query = "SELECT {$sqlFields}\n FROM `{$table}`{$sqlWhere}{$sqlOrder}{$sqlLimit};";
+
+    // \Chrissileinus\React\Log\Writer::debug("SQL:" . PHP_EOL . $query, 'DataBase');
+    return self::query($query);
+  }
+
+  static protected function genWhere($where, bool $noPrefix = false): string
+  {
+    if (!is_array($where)) return "";
+    $where = array_filter($where, 'is_string', ARRAY_FILTER_USE_KEY);
+    if (!$where) return "";
+
+    $assembleEntry = function ($key, $value) {
+      if (preg_match("/^(?<name>[^\[\]]+)(?:\[(?<condition>.+)\]){0,1}$/", $key, $match)) {
+        extract($match);
+        $condition = isset($condition) ? $condition : "=";
+        return " `{$name}` {$condition} " . quote($value);
+      }
+    };
+
+    $condition = 'AND';
+    $firstKey = array_key_first($where);
+    if (
+      $firstKey == 'AND' ||
+      $firstKey == 'OR'
+    ) {
+      $condition = $firstKey;
+      $where = array_filter($where[$condition], 'is_string', ARRAY_FILTER_USE_KEY);
+    }
+    if (!$where) return "";
+
+    $return = $noPrefix ? "" : "\n WHERE";
+    if (count($where) == 1) {
+      $key = array_key_first($where);
+      return $return . $assembleEntry($key, $where[$key]);
+    }
+
+    $return .= "\n  (";
+    $first = true;
+    foreach ($where as $key => $value) {
+      if (!$first) $return .= " {$condition}";
+
+      if (is_array($value)) {
+        if ($key == 'AND' || $key == 'OR') {
+          $return .= self::genWhere([$key => $value], true);
+          $first = false;
+        }
+        continue;
+      }
+      if (is_object($value)) continue;
+      if (is_bool($value)) continue;
+
+      $return .= $assembleEntry($key, $value);
+      $first = false;
+    }
+    $return .= " )";
+    return $return;
+  }
+
+  static protected function genLimit($limit): string
+  {
+    if (!$limit) return "";
+
+    if (is_array($limit)) {
+      $limit = array_filter($limit, 'is_int', ARRAY_FILTER_USE_KEY);
+      if (count($limit) == 1) $limit = $limit[0];
+
+      if (count($limit) > 1) return "\n LIMIT {$limit[0]} {$limit[1]}";
+    }
+
+    if (is_int($limit)) return "\n LIMIT {$limit}";
+  }
+}
---
+++ src/Connection.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * Created on Fri Mar 04 2022
+ *
+ * Copyright (c) 2022 Christian Backus (Chrissileinus)
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Chrissileinus\React\SQLite;
+
+use Clue\React\SQLite;
+
+/**
+ * A single React\MySQL connection
+ */
+class Connection implements \Evenement\EventEmitterInterface
+{
+  use \Evenement\EventEmitterTrait;
+
+  protected $connection;
+  protected $onError;
+
+  function __construct(string $dbFile, callable $onError = null)
+  {
+    $this->onError = $onError;
+
+    $sqlFactory = new SQLite\Factory();
+
+    $this->connection = $sqlFactory->openLazy($dbFile);
+    $this->connection->on('error', function ($e) {
+      $this->emit('error', [$e]);
+    });
+    $this->connection->on('close', function () {
+      $this->emit('close');
+    });
+    $this->connection->exec('PRAGMA journal_mode = WAL;');
+    $this->connection->exec('PRAGMA synchronous = NORMAL;');
+    $this->connection->query('PRAGMA database_list;');
+  }
+
+  public function query($sql, $params = []): \React\Promise\PromiseInterface
+  {
+    return $this->connection->query($sql, $params)->then(
+      function (SQLite\Result $result) {
+        return $result;
+      },
+      function (\Throwable $th) use ($sql) {
+        if (is_callable($this->onError)) {
+          return call_user_func($this->onError, $th, $sql);
+        }
+        throw $th;
+      }
+    );
+  }
+
+  public function quit()
+  {
+    return $this->connection->quit();
+  }
+
+  public function close()
+  {
+    return $this->connection->close();
+  }
+}
